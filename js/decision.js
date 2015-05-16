790568615c7807284c54eb8e0e850d226cf7d0c5
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-05-16 07:27:34 -0500

    added a slideshow

--- js/decision.js
+++ js/decision.js
@@ -1,15 +1,13 @@
 Parse.initialize("LKT2P8orJ6egkhiXZqL7xAo1xkqqg8tkd8pzjbDE", "I7ilbHUSOpCSeRKuXwN8a9YBA7KZXk06J30tS00R");
 
 $(document).ready(function() {
-	$('h1').click(function() {
-		alert("yikes!");
-	});
 	$('.menuChoices').hide();
 	bindClickFunctions();
 });
 
 var bindClickFunctions = function() {
 	$('.submit').click(function() {
+		$('.about').fadeOut();
 		$('.userInput').fadeOut(400, showMenus($('.money').val()));
 	});
 
