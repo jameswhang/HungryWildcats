894d861ec901399833d1d48c3c42171ea2579cbc
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-05-16 02:18:51 -0500

    made this a web app

---
+++ js/decision.js
@@ -0,0 +1,7 @@
+Parse.initialize("LKT2P8orJ6egkhiXZqL7xAo1xkqqg8tkd8pzjbDE", "I7ilbHUSOpCSeRKuXwN8a9YBA7KZXk06J30tS00R");
+
+$(".submit").click(function() {
+	console.log()
+	$(".userInput").hide();
+	$(".menuChoices").show();
+});
