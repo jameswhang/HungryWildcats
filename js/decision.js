c1b6916ddc47bb3707ccd5b9086ce9f70e39e944
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-05-16 02:41:04 -0500

    hooked up with parse

--- js/decision.js
+++ js/decision.js
@@ -1,7 +1,35 @@
 Parse.initialize("LKT2P8orJ6egkhiXZqL7xAo1xkqqg8tkd8pzjbDE", "I7ilbHUSOpCSeRKuXwN8a9YBA7KZXk06J30tS00R");
 
-$(".submit").click(function() {
-	console.log()
-	$(".userInput").hide();
-	$(".menuChoices").show();
+$(document).ready(function() {
+	$(".menuChoices").hide();
 });
+
+$(".submit").click(function() {
+	$(".userInput").fadeOut(400, showMenus($(".money").val()));
+});
+
+var showMenus = function(money) {
+	var menu = Parse.Object.extend("Menu");
+	var query = new Parse.Query(menu);
+	query.find({
+		success: function(results) {
+			var numResults = results.length;
+			for (var i = 0; i < numResults; i++) {
+				var aMenu = results[i];
+				var mPrice = aMenu._serverData.Price;
+				var mName = aMenu._serverData.Name;
+				var mRes = aMenu._serverData.Restaurant;
+
+				//console.log(money);
+				//console.log(mPrice);
+				//console.log(mName);
+				//console.log(mRes);
+			}
+		},
+		error: function(error) {
+			alert("shit");
+		}
+	})
+
+	$(".menuChoices").fadeIn();
+}
