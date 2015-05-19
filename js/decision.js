ee003a00ad466192884d677ec2a8089186ca5ddc
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-05-19 02:12:36 -0500

    made the key more secure

--- js/decision.js
+++ js/decision.js
@@ -1,5 +1,3 @@
-Parse.initialize("LKT2P8orJ6egkhiXZqL7xAo1xkqqg8tkd8pzjbDE", "I7ilbHUSOpCSeRKuXwN8a9YBA7KZXk06J30tS00R");
-
 $(document).ready(function() {
 	$('.menuChoices').hide();
 	bindClickFunctions();
