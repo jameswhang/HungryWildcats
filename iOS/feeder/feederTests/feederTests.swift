5db58816d4bd24621af7339312b0425828ec442b
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2015-07-22 11:27:38 -0500

    Initial Commit

---
+++ iOS/feeder/feederTests/feederTests.swift
@@ -0,0 +1,36 @@
+//
+//  feederTests.swift
+//  feederTests
+//
+//  Created by jameswhang on 7/22/15.
+//  Copyright (c) 2015 jameswhang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class feederTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
